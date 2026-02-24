d0e6acb9673e598a1675fc24a7a1989f37fcd244
Hussein Mohamud <huseinmohamud33@gmail.com>
2026-02-24 10:38:46 +0300

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -403,6 +403,6 @@ def contact():
 
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 10000))
-    app.run(
-        host="0.0.0.0",port=port )
+    port = int(os.environ.get("PORT", 3402))
+    app.run(host="0.0.0.0",port=port )
+
