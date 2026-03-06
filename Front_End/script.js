5ef8ada717d5aec9bb41ee8dd50bda35f43a4334
Hussein Mohamud <huseinmohamud33@gmail.com>
2026-03-06 23:07:43 +0300

    Update script.js

--- Front_End/script.js
+++ Front_End/script.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'https://tafaftire-detection-system.onrender.com';
+const API_BASE_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+    ? 'http://127.0.0.1:3402'
+    : 'https://tafaftire-detection-system.onrender.com';
 
 window.isAnalyzing = false;
 
